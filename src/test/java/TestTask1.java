86b484ca8b6742210adb3c9a6901d0a171c49e6a
Yuriy_Nikitchenko <yuriy_nikitchenko@epam.com>
2021-12-05 14:26:45 +0200

    solve for task1 added; redundant JUNIT integration class deleted

---
+++ src/test/java/TestTask1.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTask1 {
+    @Test
+    public void testGetSum() {
+        assertEquals(110, Task1.getSum());
+    }
+
+}
