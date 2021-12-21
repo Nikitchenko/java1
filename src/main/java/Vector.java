be3e9d7224f107bbbaf7bfb71013d74ad1187292
Yuriy_Nikitchenko <yuriy_nikitchenko@epam.com>
2021-12-21 10:37:58 +0200

    refactor of task3.1 - Vector

--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -12,51 +12,52 @@ public class Vector {
     }
 
     public static void main(String[] args) {
-
         Vector myVector1 = new Vector(1, 2, 3);
         Vector myVector2 = new Vector(2, 2, 2);
-
         System.out.println("My 1st Vector: " + myVector1);
         System.out.println("My 2nd Vector: " + myVector2);
-
         System.out.println("Length of my 1st Vector: " + myVector1.calculateVectorLength());
         System.out.println("Length of my 2nd Vector: " + myVector2.calculateVectorLength());
-
         System.out.println("Dot product (scalar multiplication) of my 1st and 2nd Vectors: " +
                 calculateDotProduct(myVector1, myVector2));
         System.out.println("Dot product (scalar multiplication) of random Vectors: " +
                 calculateDotProduct());
-
-
     }
 
-    // Such implementation of calculation dot product requires 2 arguments
+    /**
+     * Such implementation of calculation dot product requires 2 arguments
+     *
+     * @param vector1
+     * @param vector2
+     * @return scalar multiplication
+     */
     public static double calculateDotProduct(Vector vector1, Vector vector2) {
         return vector1.getCoordX() * vector2.getCoordX() +
                 vector1.getCoordY() * vector2.getCoordY() +
                 vector1.getCoordZ() * vector2.getCoordZ();
     }
 
-
-    // "Overloaded" method for calculation dot product. It uses random Vectors
+    /**
+     * "Overloaded" method for calculation dot product. It uses random Vectors
+     *
+     * @return scalar multiplication
+     */
     public static double calculateDotProduct() {
         return calculateDotProduct(generateRandomVectors()[1], generateRandomVectors()[2]);
     }
 
     public static Vector[] generateRandomVectors() {
-
         int vectorsQuantity = 3;
         Vector[] randomVectors = new Vector[vectorsQuantity];
         for (int i = 0; i < vectorsQuantity; i++) {
             double[] anArray = new Random().doubles(3, 0, 10).toArray();
             randomVectors[i] = new Vector(anArray[0], anArray[1], anArray[2]);
         }
-
         return randomVectors;
     }
 
     public double calculateVectorLength() {
-        return Math.sqrt(Math.pow(this.getCoordX(), 2) + Math.pow(this.getCoordY(), 2) + Math.pow(this.getCoordZ(), 2));
+        return Math.sqrt(Math.pow(coordX, 2) + Math.pow(coordY, 2) + Math.pow(coordZ, 2));
     }
 
     @Override
--- src/test/java/TestVector.java
+++ src/test/java/TestVector.java
@@ -9,26 +9,23 @@ public class TestVector {
     Vector testVector2 = new Vector(2, 2, 2);
     Vector testVector3 = new Vector(7, 8, 9);
 
-
     @Test
     public void testVector() {
 
         // check that constructor works well
-        assertEquals(8, testVector3.getCoordY());
+        assertEquals(8, testVector3.getCoordY(), "Not expected coordY in new vector");
         testVector3.setCoordY(10);
-        assertEquals(10, testVector3.getCoordY());
+        assertEquals(10, testVector3.getCoordY(), "Not expected coordY in the vector after change");
     }
 
     @Test
     public void testCalculateVectorLength() {
-
-        assertEquals(3.7416573867739413, testVector1.calculateVectorLength());
+        assertEquals(3.7416573867739413, testVector1.calculateVectorLength(), "incorrect vector's length");
     }
 
     @Test
     public void testCalculateDotProduct() {
-
-        assertEquals(12.00d, Vector.calculateDotProduct(testVector1, testVector2));
+        assertEquals(12.00d, Vector.calculateDotProduct(testVector1, testVector2), "incorrect dot product");
     }
 
     @Test
@@ -36,16 +33,19 @@ public class TestVector {
 
         // Our random Vectors have coordinates in range 0<coordinate<100
         // let`s check some of them
-        assertTrue(0.00d <= Vector.generateRandomVectors()[0].getCoordX() && Vector.generateRandomVectors()[0].getCoordY() <= 100.00d);
-        assertTrue(0.00d <= Vector.generateRandomVectors()[1].getCoordY() && Vector.generateRandomVectors()[2].getCoordZ() <= 100.00d);
+        assertTrue(0.00d <= Vector.generateRandomVectors()[0].getCoordX() &&
+                Vector.generateRandomVectors()[0].getCoordY() <= 100.00d, "not expected coordY in new random vector");
+        assertTrue(0.00d <= Vector.generateRandomVectors()[1].getCoordY() &&
+                Vector.generateRandomVectors()[2].getCoordZ() <= 100.00d, "not expected coordZ in new random vector");
     }
 
     @Test
     public void testCalculateDotProductRandom() {
 
         // Because our random Vectors have coordinates in range 0<coordinate<100
-        // assert that dot product will be in range 0<value<250
-        assertTrue(0.00d <= Vector.calculateDotProduct() && Vector.calculateDotProduct() <= 300.00d);
+        // assert that dot product will be in range 0<=value<=300
+        assertTrue(0.00d <= Vector.calculateDotProduct() &&
+                Vector.calculateDotProduct() <= 300.00d, "not expected dot product of random vectors");
     }
 
 }
