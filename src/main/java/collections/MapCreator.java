6d737e67c3f0599f94838694415d5b047183b713
Yuriy_Nikitchenko <yuriy_nikitchenko@epam.com>
2021-12-29 11:31:34 +0200

    task 4.5 implementation

---
+++ src/main/java/collections/MapCreator.java
@@ -0,0 +1,79 @@
+package collections;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+
+public class MapCreator {
+    public static void main(String[] args) {
+
+        List<String> myStrings = Arrays.asList("foo", "bar", "delta", "gamma");
+        List<Integer> myNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        System.out.print("1st List: ");
+        System.out.println(myStrings);
+        System.out.print("2nd List: ");
+        System.out.println(myNumbers);
+
+        System.out.println("\nOption 1:");
+        System.out.println(createMapOp1(myNumbers, myStrings));
+        System.out.println(createMapOp1(myStrings, myNumbers));
+
+        System.out.println("\nOption 2:");
+        System.out.println(createMapOp2(myNumbers, myStrings));
+        System.out.println();
+        System.out.println(createMapOp2(myStrings, myNumbers));
+    }
+
+    /**
+     * the Method "Option 1" without error interception (null for extra keys, extra values ignored)
+     * @param keys
+     * @param values
+     * @param <K>
+     * @param <V>
+     * @return the Map
+     */
+    public static <K, V> TreeMap<K, V> createMapOp1(List<K> keys, List<V> values) {
+
+        TreeMap<K, V> theMap = new TreeMap<K, V>();
+        for (int i = 0; i < keys.size(); i++) {
+
+            if (values.size() <= i) {
+                theMap.put(keys.get(i), null);
+            } else {
+                theMap.put(keys.get(i), values.get(i));
+            }
+        }
+        return theMap;
+    }
+
+    /**
+     * the Method "Option 2" with error interception when size K != V
+     * @param keys
+     * @param values
+     * @param <K>
+     * @param <V>
+     * @return the Map
+     */
+    public static <K, V> TreeMap<K, V> createMapOp2(List<K> keys, List<V> values) {
+
+        TreeMap<K, V> theMap = new TreeMap<K, V>();
+        for (int i = 0; i < keys.size(); i++) {
+
+            // Use try-catch to intercept error in the case K > V
+            try {
+                theMap.put(keys.get(i), values.get(i));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Value is not enough for map.");
+            }
+        }
+
+        // The case when K < V we will implement in slightly differ way
+        if (values.size() - keys.size() > 0) {
+            int extraValues = values.size() - keys.size();
+            System.out.println("Key is not enough for map.\nThere are " + extraValues + " extra values.");
+        }
+        return theMap;
+    }
+
+}
---
+++ src/test/java/TestMapCreator.java
@@ -0,0 +1,88 @@
+import collections.MapCreator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMapCreator {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    List<String> myTestStrings = Arrays.asList("foo", "bar", "delta");
+    List<Integer> myTestNumbers = Arrays.asList(1, 2, 3, 4);
+    TreeMap<String, Integer> testMapKV_1 = new TreeMap<String, Integer>() {{
+        put("foo", 1);
+        put("bar", 2);
+        put("delta", 3);
+    }};
+
+    @Test
+    public void testCreateMapOp1_1() {
+        //Option 1, K < V
+        assertEquals(testMapKV_1, MapCreator.createMapOp1(myTestStrings, myTestNumbers), "Maps are not equal.");
+    }
+
+    @Test
+    public void testCreateMapOp1_2() {
+        TreeMap<Integer, String> testMapKV1_2 = new TreeMap<Integer, String>() {{
+            put(1, "foo");
+            put(2, "bar");
+            put(3, "delta");
+            put(4, null);
+        }};
+        //Option 1, K > V
+        assertEquals(testMapKV1_2, MapCreator.createMapOp1(myTestNumbers, myTestStrings), "Maps are not equal.");
+    }
+
+    @Test
+    public void testCreateMapOp2_1() {
+        //Option 2, K < V
+        assertEquals(testMapKV_1, MapCreator.createMapOp2(myTestStrings, myTestNumbers), "Maps are not equal.");
+    }
+
+    @Test
+    public void testCreateMapOp2_2() {
+        TreeMap<Integer, String> testMapKV2_2 = new TreeMap<Integer, String>() {{
+            put(1, "foo");
+            put(2, "bar");
+            put(3, "delta");
+        }};
+        //Option 2, K > V
+        assertEquals(testMapKV2_2, MapCreator.createMapOp2(myTestNumbers, myTestStrings), "Maps are not equal.");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void testMessageInConsoleOp2_1() {
+        //Option 2, K < V
+        MapCreator.createMapOp2(myTestStrings, myTestNumbers);
+        assertEquals("Key is not enough for map.\nThere are 1 extra values.", outputStreamCaptor.toString()
+                .trim(), "Message is incorrect or not displayed.");
+    }
+
+    @Test
+    void testMessageInConsoleOp2_2() {
+        //Option 2, K > V
+        MapCreator.createMapOp2(myTestNumbers, myTestStrings);
+        assertEquals("Value is not enough for map.", outputStreamCaptor.toString()
+                .trim(), "Message is incorrect or not displayed.");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+}
