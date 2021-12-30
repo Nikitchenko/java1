081b7d820916c97406d5ac26c8eba934c33c3739
Yuriy_Nikitchenko <yuriy_nikitchenko@epam.com>
2021-12-30 20:25:48 +0200

    FrequencyCounter refactored

--- src/main/java/collections/FrequencyCounter.java
+++ src/main/java/collections/FrequencyCounter.java
@@ -1,5 +1,6 @@
 package collections;
 
+import java.util.Map;
 import java.util.TreeMap;
 
 public class FrequencyCounter {
@@ -11,9 +12,9 @@ public class FrequencyCounter {
         System.out.println("\nFrequency of characters:\n" + countCharFrequency(text));
     }
 
-    public static TreeMap countCharFrequency(String text) {
+    public static Map countCharFrequency(String text) {
 
-        TreeMap<String, Integer> frequencyDictionary = new TreeMap<String, Integer>();
+        Map<String, Integer> frequencyDictionary = new TreeMap<String, Integer>();
 
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
