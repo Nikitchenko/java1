c1a979809f352eb113cd85fa6e6a61a2c66771f4
Yuriy_Nikitchenko <yuriy_nikitchenko@epam.com>
2022-01-05 18:51:15 +0200

    change argument Array to Collection

--- src/main/java/collections/CollectionWithoutDuplicates.java
+++ src/main/java/collections/CollectionWithoutDuplicates.java
@@ -2,28 +2,30 @@ package collections;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.HashSet;
 
 public class CollectionWithoutDuplicates {
     public static void main(String[] args) {
 
         String[] myStrings = {"string1", "string2", "string3", "string1", "string5"};
+        Collection<String> myStringsCollection = Arrays.asList(myStrings);
         Integer[] myNumbers = {1, 2, 3, 1, 5};
+        Collection<Integer> myNumbersCollection = Arrays.asList(myNumbers);
 
-        System.out.println("\nArray of Strings objects:");
-        System.out.println(Arrays.toString(myStrings));
+        System.out.println("\nCollection of Strings objects:");
+        System.out.println(myStringsCollection);
         System.out.println("\nSet of Strings (without duplicates):");
-        System.out.println(removeDuplicates(myStrings));
+        System.out.println(removeDuplicates(myStringsCollection));
 
-        System.out.println("\nArray of Integers objects: ");
-        System.out.println(Arrays.toString(myNumbers));
-        System.out.println("\nSet of Integer (without duplicates):");
-        System.out.println(removeDuplicates(myNumbers));
+        System.out.println("\nCollection of Integers objects: ");
+        System.out.println(myNumbersCollection);
+        System.out.println("\nSet of Integers (without duplicates):");
+        System.out.println(removeDuplicates(myNumbersCollection));
 
     }
 
-    public static <V> Collection<V> removeDuplicates(V[] aCollection) {
-        return Arrays.stream(aCollection).collect(Collectors.toSet());
+    public static <V> Collection<V> removeDuplicates(Collection aCollection) {
+        return new HashSet<>(aCollection);
     }
 
 }
--- src/test/java/TestCollectionWithoutDuplicates.java
+++ src/test/java/TestCollectionWithoutDuplicates.java
@@ -2,6 +2,7 @@ import collections.CollectionWithoutDuplicates;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,14 +12,17 @@ public class TestCollectionWithoutDuplicates {
     @Test
     public void testRemoveDuplicates() {
         String[] myTestStringsDuplicated = {"string1", "string2", "string3", "string1", "string5"};
+        Collection<String> myTestStringsCollection = Arrays.asList(myTestStringsDuplicated);
         Integer[] myTestNumbersDuplicated = {1, 2, 3, 1, 5};
+        Collection<Integer> myTestNumbersCollection = Arrays.asList(myTestNumbersDuplicated);
+
         String[] myTestStrings = {"string1", "string2", "string3", "string5"};
         HashSet<String> mySetStrings = new HashSet<>(Arrays.asList(myTestStrings));
         Integer[] myTestNumbers = {1, 2, 3, 5};
         HashSet<Integer> mySetNumbers = new HashSet<>(Arrays.asList(myTestNumbers));
 
-        assertTrue(mySetStrings.equals(CollectionWithoutDuplicates.removeDuplicates(myTestStringsDuplicated)), "Collections are not equal.");
-        assertTrue(mySetNumbers.equals(CollectionWithoutDuplicates.removeDuplicates(myTestNumbersDuplicated)), "Collections are not equal.");
+        assertTrue(mySetStrings.equals(CollectionWithoutDuplicates.removeDuplicates(myTestStringsCollection)), "Collections are not equal.");
+        assertTrue(mySetNumbers.equals(CollectionWithoutDuplicates.removeDuplicates(myTestNumbersCollection)), "Collections are not equal.");
     }
 
 }
